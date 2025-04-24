feedcaead5444246b8244c35549cca12518e8950
comnk <vhsiao36@gmail.com>
2025-04-24 00:53:57 -0700

    basic modal setup with shadcn

---
+++ src/app/components/LoginModal/LoginModal.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from '@/components/ui/dialog';
+import { Dispatch, SetStateAction } from 'react';
+
+interface LoginModalProps {
+  open: boolean;
+  onOpenChange: Dispatch<SetStateAction<boolean>>;
+}
+
+export default function LoginModal({ open, onOpenChange }: LoginModalProps) {
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you are done.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/app/components/SignInButton.tsx
+++ src/app/components/SignInButton.tsx
@@ -1,19 +1,24 @@
-import { memo } from 'react';
+import { useState, memo } from 'react';
 import ResetZoomIcon from '../assets/ResetZoomIcon';
 import GlassButton from './GlassButton/GlassButton';
+import LoginModal from './LoginModal/LoginModal';
 
 function SignInButton() {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
-    <GlassButton
-      onClick={e => {}}
-      onMouseDown={e => {
-        e.stopPropagation();
-      }}
-      text="Sign In"
-      className="fixed right-9 top-5 z-[4]"
-    >
-      {<ResetZoomIcon />}
-    </GlassButton>
+    <>
+      <GlassButton
+        onClick={() => setModalOpen(true)}
+        onMouseDown={e => e.stopPropagation()}
+        text="Sign In"
+        className="fixed right-9 top-5 z-[4]"
+      >
+        <ResetZoomIcon />
+      </GlassButton>
+
+      <LoginModal open={modalOpen} onOpenChange={setModalOpen} />
+    </>
   );
 }
 
