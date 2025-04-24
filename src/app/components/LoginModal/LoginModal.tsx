7a1708c3275c045dc066bac35fa3311cd550ebb8
comnk <vhsiao36@gmail.com>
2025-04-24 11:05:45 -0700

    added styling to login modal

--- src/app/components/LoginModal/LoginModal.tsx
+++ src/app/components/LoginModal/LoginModal.tsx
@@ -1,6 +1,7 @@
-import { Button } from '@/components/ui/button';
+import './LoginModal.scss';
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -8,9 +9,10 @@ import {
   DialogTitle
 } from '@/components/ui/dialog';
 
-import { authClient } from '../../../lib/auth-client';
-import { Dispatch, SetStateAction, useState } from 'react';
-import { useRouter } from 'next/navigation';
+//import { authClient } from '../../../lib/auth-client';
+import { Dispatch, SetStateAction } from 'react';
+//import { useRouter } from 'next/navigation';
+import GlassButton from '../GlassButton/GlassButton';
 
 interface LoginModalProps {
   open: boolean;
@@ -18,48 +20,60 @@ interface LoginModalProps {
 }
 
 export default function LoginModal({ open, onOpenChange }: LoginModalProps) {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
-  const router = useRouter();
+  //   const [loading, setLoading] = useState(false);
+  //   const [error, setError] = useState('');
+  //   const router = useRouter();
 
-  const handleGoogleSignIn = async () => {
-    setLoading(true);
-    setError('');
+  //   const handleGoogleSignIn = async () => {
+  //     setLoading(true);
+  //     setError('');
 
-    try {
-      const { error } = await authClient.signIn.social(
-        {
-          provider: 'google',
-          callbackURL: '/dashboard'
-        },
-        {
-          onRequest: () => setLoading(true),
-          onSuccess: () => router.push('/dashboard'),
-          onError: ctx => setError(ctx.error.message || 'Sign in failed')
-        }
-      );
+  //     try {
+  //       const { error } = await authClient.signIn.social(
+  //         {
+  //           provider: 'google',
+  //           callbackURL: '/dashboard'
+  //         },
+  //         {
+  //           onRequest: () => setLoading(true),
+  //           onSuccess: () => router.push('/dashboard'),
+  //           onError: ctx => setError(ctx.error.message || 'Sign in failed')
+  //         }
+  //       );
 
-      if (error) {
-        setError(error.message || 'Sign in failed');
-      }
-    } catch {
-      setError('An unexpected error occurred');
-    } finally {
-      setLoading(false);
-    }
-  };
+  //       if (error) {
+  //         setError(error.message || 'Sign in failed');
+  //       }
+  //     } catch {
+  //       setError('An unexpected error occurred');
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you are done.
-          </DialogDescription>
+          <DialogTitle className="dialog-title">Want to save your creation?</DialogTitle>
+          <DialogDescription className="dialog-description">You need to log in</DialogDescription>
         </DialogHeader>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
+        <DialogFooter className="modal-footer">
+          <DialogClose asChild>
+            <GlassButton
+              onClick={() => {}}
+              onMouseDown={e => e.stopPropagation()}
+              text="I Just Want to Mess Around"
+              className="modal-button justify-center"
+            ></GlassButton>
+          </DialogClose>
+          <GlassButton
+            onClick={() => {}}
+            onMouseDown={e => e.stopPropagation()}
+            text="Log In Via Email"
+            className="modal-button justify-center"
+            color="dark"
+          ></GlassButton>
         </DialogFooter>
       </DialogContent>
     </Dialog>
