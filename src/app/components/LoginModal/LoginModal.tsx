55ddf286981ff502dccd8c87d50ba2b4a8ed9d97
comnk <vhsiao36@gmail.com>
2025-04-28 22:57:14 -0700

    first fix to CI issues

--- src/app/components/LoginModal/LoginModal.tsx
+++ src/app/components/LoginModal/LoginModal.tsx
@@ -25,7 +25,6 @@ export default function LoginModal({ open, onOpenChange }: LoginModalProps) {
   const router = useRouter();
 
   const handleGoogleSignIn = async () => {
-    console.log('HELLO!!!');
     setLoading(true);
     setError('');
 
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from 'better-auth';
 import { MongoClient } from 'mongodb';
 import { mongodbAdapter } from 'better-auth/adapters/mongodb';
-import { createAuthMiddleware, APIError } from 'better-auth/api';
+import { createAuthMiddleware } from 'better-auth/api';
 
 const client = new MongoClient(process.env.DATABASE_URL as string);
 const db = client.db();
@@ -13,32 +13,5 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     }
   },
-  database: mongodbAdapter(db),
-  hooks: {
-    before: createAuthMiddleware(async ctx => {
-      if (ctx.path !== '/sign-in/social') {
-        return;
-      }
-
-      console.log(ctx);
-      // if (!ctx.body?.email.endsWith("@ucsd.edu")) {
-      //     throw new APIError("BAD_REQUEST", {
-      //         message: "Email must end with @example.com",
-      //     });
-      // }
-    }),
-
-    after: createAuthMiddleware(async ctx => {
-      if (ctx.path !== '/sign-in/social') {
-        return;
-      }
-
-      console.log(ctx.context.newSession);
-      //   if (!ctx.body?.email.endsWith('@ucsd.edu')) {
-      //     throw new APIError('BAD_REQUEST', {
-      //       message: 'Email must end with @example.com'
-      //     });
-      //   }
-    })
-  }
+  database: mongodbAdapter(db)
 });
