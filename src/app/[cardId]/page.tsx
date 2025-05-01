be104c332841ce068120334347a71bebca48ef19
EdwardNew <pkarrows@gmail.com>
2025-04-30 17:35:27 -0700

    feat: added drawer for mobile and made layout responsive

--- src/app/[cardId]/page.tsx
+++ src/app/[cardId]/page.tsx
@@ -45,16 +45,18 @@ export default function Page() {
   setBorderStyle(card.borderStyle);
 
   return (
-    <div className="flex h-screen w-screen items-center justify-center gap-16 bg-[#eaeaea]">
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-16 bg-[#eaeaea] px-5 md:flex-row">
       <div style={{ width: 300, height: 400 }}>
         <SimplifiedCard id={card.id} />
       </div>
 
-      <div className="flex h-[380px] w-[500px] items-center justify-center rounded-[45px] bg-[#f5f5f5] p-8">
+      <div className="hidden h-[380px] w-[500px] items-center justify-center rounded-[45px] bg-[#f5f5f5] p-8 md:flex">
         <CustomizationPanel />
       </div>
 
-      {/* <CustomizationDrawer /> */}
+      <div className="md:hidden">
+        <CustomizationDrawer />
+      </div>
     </div>
   );
 }
