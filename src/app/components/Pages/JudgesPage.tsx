73787afe1226141b695439b99077262b9c09dfb9
Sahil Gathe <sahilsankur@gmail.com>
2025-05-03 10:13:58 -0700

    feat: beveled judges

--- src/app/components/Pages/JudgesPage.tsx
+++ src/app/components/Pages/JudgesPage.tsx
@@ -9,6 +9,18 @@ type JudgesPageProps = {
 };
 
 export default function JudgesPage({ ref, showExpanded, borderStyle }: JudgesPageProps) {
+  function renderPrimaryTriangles() {
+    if (borderStyle !== 'beveled') return null;
+    return (
+      <>
+        <div className="primary-triangle absolute left-0 top-0"></div>
+        <div className="primary-triangle absolute right-0 top-0 rotate-90"></div>
+        <div className="primary-triangle absolute bottom-0 left-0 rotate-[270deg]"></div>
+        <div className="primary-triangle absolute bottom-0 right-0 rotate-180"></div>
+      </>
+    );
+  }
+
   const varBorderRadius =
     borderStyle === 'rounded'
       ? 'rounded-xl'
@@ -59,16 +71,19 @@ export default function JudgesPage({ ref, showExpanded, borderStyle }: JudgesPag
     return (
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
         {judges.map((judge, index) => (
-          <div key={index} className={`flex flex-col items-center ${varBorderRadius}`}>
-            <Image
-              width={200}
-              height={250}
-              src={judge.image}
-              alt={`${judge.first} ${judge.last}`}
-              className={`h-[250px] w-[200px] pb-2 ${varBorderRadius}`}
-            />
-            <h3 className="text-lg font-bold">{`${judge.first} ${judge.last}`}</h3>
+          <div key={index} className={`flex flex-col items-center`}>
+            <div className={`relative bg-[var(--card-accent-color)] ${varBorderRadius}`}>
+              {renderPrimaryTriangles()}
+              <Image
+                width={200}
+                height={250}
+                src={judge.image}
+                alt={`${judge.first} ${judge.last}`}
+                className={`h-[250px] w-[200px] pb-2 ${varBorderRadius}`}
+              />
+            </div>
             <p className="text-sm">{judge.position}</p>
+            <h3 className="text-lg font-bold">{`${judge.first} ${judge.last}`}</h3>
           </div>
         ))}
       </div>
