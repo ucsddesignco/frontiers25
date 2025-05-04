6e19747de75022a3f1593a68f2d483090f41e740
comnk <vhsiao36@gmail.com>
2025-05-04 08:55:48 -0700

    added styling to the sections dropdown

---
+++ src/app/assets/AgendaIcon.tsx
@@ -0,0 +1,15 @@
+export default function AgendaIcon() {
+  return (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M0.25 2.25H15.75V15.75H0.25V2.25ZM1.75 3.75V14.25H14.25V3.75H1.75Z"
+        fill="#1A1622"
+      />
+      <path fillRule="evenodd" clipRule="evenodd" d="M10.25 5V0.5H11.75V5H10.25Z" fill="#1A1622" />
+      <path fillRule="evenodd" clipRule="evenodd" d="M1 7.25H15V8.75H1V7.25Z" fill="#1A1622" />
+      <path fillRule="evenodd" clipRule="evenodd" d="M4.25 5V0.5H5.75V5H4.25Z" fill="#1A1622" />
+    </svg>
+  );
+}
