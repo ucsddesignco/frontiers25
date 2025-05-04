a7b7330bcd39eb66bffc28faff1aa7306fbe5a3f
Sahil Gathe <sahilsankur@gmail.com>
2025-05-03 23:00:08 -0700

    agenda content set

--- src/app/components/Pages/AgendaPage.tsx
+++ src/app/components/Pages/AgendaPage.tsx
@@ -44,71 +44,55 @@ export default function AgendaPage({ ref, showExpanded, borderStyle }: AgendaPag
   function renderDay1() {
     return (
       <div className="relative flex h-full w-full flex-col items-center justify-between gap-5">
-        <div className="absolute left-1/2 top-0 z-0 h-[90%] w-[4px] bg-[var(--card-accent-color)]"></div>
+        <div className="absolute left-1/2 top-0 z-0 h-[85%] w-[4px] bg-[var(--card-accent-color)]"></div>
         <div
           className={`z-10 flex h-[80%] w-[85%] flex-col items-center justify-between gap-10 ${varBorderRadius}`}
         >
           {/* Item 1 */}
-          <div className={`relative bg-[var(--card-primary-color)]`}>
+          <div className={`relative mb-[15%] bg-[var(--card-primary-color)]`}>
             {renderAccentTriangles()}
             <div
-              className={`overflow-clip border-[4px] border-[var(--card-accent-color)] p-5 text-center ${varBorderRadius}`}
+              className={`items-center overflow-clip border-[4px] border-[var(--card-accent-color)] p-10 text-center ${varBorderRadius}`}
             >
-              <p>7:00 PM</p>
+              <p className="text-center text-[1.3rem] font-bold uppercase">7:00 PM</p>
               <h1 className="text-center text-[2rem] font-bold uppercase leading-[1.25] lg:text-[3rem]">
                 Event Kickoff
               </h1>
-              <p className="text-center text-[1rem] font-bold uppercase">
+              <p>
                 Designers will begin with registration and sign-in, an introduction to the event,
                 and energizing lighting talks.
               </p>
             </div>
           </div>
           {/* Item 2 */}
-          <div className={`relative bg-[var(--card-primary-color)]`}>
+          <div className={`relative mb-[15%] bg-[var(--card-primary-color)]`}>
             {renderAccentTriangles()}
             <div
-              className={`overflow-clip border-[4px] border-[var(--card-accent-color)] p-5 text-center ${varBorderRadius}`}
+              className={`items-center overflow-clip border-[4px] border-[var(--card-accent-color)] p-10 text-center ${varBorderRadius}`}
             >
-              <p>7:30 PM</p>
-              <h1 className="text-center text-[2rem] font-bold uppercase leading-[1.25] lg:text-[3rem]">
-                Lighting Talks
-              </h1>
-              <p className="text-center text-[1rem] font-bold uppercase">
-                Designers will begin with registration and sign-in, an introduction to the event,
-                and energizing lighting talks.
-              </p>
-            </div>
-          </div>
-          {/* Item 3 */}
-          <div className={`relative bg-[var(--card-primary-color)]`}>
-            {renderAccentTriangles()}
-            <div
-              className={`overflow-clip border-[4px] border-[var(--card-accent-color)] p-5 text-center ${varBorderRadius}`}
-            >
-              <p className="text-center text-[1rem] font-bold uppercase">8:00 PM</p>
+              <p className="text-center text-[1.3rem] font-bold uppercase">8:00 PM</p>
               <h1 className="text-center text-[2rem] font-bold uppercase leading-[1.25] lg:text-[3rem]">
                 Sprint Starts
               </h1>
               <p>
-                Designers will begin with registration and sign-in, an introduction to the event,
-                and energizing lighting talks.
+                Prompt is revealed and participants begin designing to address the prompt and
+                prepare to present their solution.
               </p>
             </div>
           </div>
-          {/* Item 4 */}
-          <div className={`relative mt-[15%] bg-[var(--card-primary-color)]`}>
+          {/* Item 3 */}
+          <div className={`relative mb-[15%] bg-[var(--card-primary-color)]`}>
             {renderAccentTriangles()}
             <div
-              className={`overflow-clip border-[4px] border-[var(--card-accent-color)] p-5 text-center ${varBorderRadius}`}
+              className={`items-center overflow-clip border-[4px] border-[var(--card-accent-color)] p-10 text-center ${varBorderRadius}`}
             >
-              <p className="text-center text-[1rem] font-bold uppercase">11:00 PM</p>
+              <p className="text-center text-[1.3rem] font-bold uppercase">9:00 PM</p>
               <h1 className="text-center text-[2rem] font-bold uppercase leading-[1.25] lg:text-[3rem]">
-                Closing Remarks
+                Day 1 Wrap-up
               </h1>
               <p>
-                Designers will begin with registration and sign-in, an introduction to the event,
-                and energizing lighting talks.
+                After some closing announcements, participants will leave to get some rest and
+                prepare for day 2!
               </p>
             </div>
           </div>
@@ -120,17 +104,17 @@ export default function AgendaPage({ ref, showExpanded, borderStyle }: AgendaPag
   function renderDay2() {
     return (
       <div className="relative flex h-full w-full flex-col items-center justify-between gap-5">
-        <div className="absolute left-1/2 top-0 z-0 h-[90%] w-[4px] bg-[var(--card-accent-color)]"></div>
+        <div className="absolute left-1/2 top-0 z-0 h-[85%] w-[4px] bg-[var(--card-accent-color)]"></div>
         <div
           className={`z-10 flex h-[80%] w-[85%] flex-col items-center justify-between gap-10 ${varBorderRadius}`}
         >
           {/* Item 1 */}
-          <div className={`relative mb-[15%] bg-[var(--card-primary-color)]`}>
+          <div className={`relative mb-[1%] w-full bg-[var(--card-primary-color)]`}>
             {renderAccentTriangles()}
             <div
-              className={`overflow-clip border-[4px] border-[var(--card-accent-color)] p-5 text-center ${varBorderRadius}`}
+              className={`items-center overflow-clip border-[4px] border-[var(--card-accent-color)] p-10 text-center ${varBorderRadius}`}
             >
-              <p className="text-center text-[1rem] font-bold uppercase">7:00 PM</p>
+              <p className="text-center text-[1.3rem] font-bold uppercase">10:00 AM</p>
               <h1 className="text-center text-[2rem] font-bold uppercase leading-[1.25] lg:text-[3rem]">
                 Event Kickoff
               </h1>
@@ -141,51 +125,58 @@ export default function AgendaPage({ ref, showExpanded, borderStyle }: AgendaPag
             </div>
           </div>
           {/* Item 2 */}
-          <div className={`relative bg-[var(--card-primary-color)]`}>
+          <div className={`relative mb-[20%] w-full bg-[var(--card-primary-color)]`}>
             {renderAccentTriangles()}
             <div
-              className={`overflow-clip border-[4px] border-[var(--card-accent-color)] p-5 text-center ${varBorderRadius}`}
+              className={`items-center overflow-clip border-[4px] border-[var(--card-accent-color)] p-10 text-center ${varBorderRadius}`}
             >
-              <p className="text-center text-[1rem] font-bold uppercase">7:30 PM</p>
-              <h1 className="text-center text-[2rem] font-bold uppercase leading-[1.25] lg:text-[3rem]">
-                Lighting Talks
-              </h1>
-              <p>
-                Designers will begin with registration and sign-in, an introduction to the event,
-                and energizing lighting talks.
-              </p>
-            </div>
-          </div>
-          {/* Item 3 */}
-          <div className={`relative bg-[var(--card-primary-color)]`}>
-            {renderAccentTriangles()}
-            <div
-              className={`overflow-clip border-[4px] border-[var(--card-accent-color)] p-5 text-center ${varBorderRadius}`}
-            >
-              <p className="text-center text-[1rem] font-bold uppercase">8:00 PM</p>
+              <p className="text-center text-[1.3rem] font-bold uppercase">10:30 AM</p>
               <h1 className="text-center text-[2rem] font-bold uppercase leading-[1.25] lg:text-[3rem]">
                 Sprint Starts
               </h1>
               <p>
-                Designers will begin with registration and sign-in, an introduction to the event,
-                and energizing lighting talks.
+                Prompt is revealed and participants begin designing to address the prompt and
+                prepare to present their solution.
               </p>
             </div>
           </div>
-          {/* Item 4 */}
-          <div className={`relative bg-[var(--card-primary-color)]`}>
+          {/* Item 3 */}
+          <div className={`relative mb-[5%] w-full bg-[var(--card-primary-color)]`}>
             {renderAccentTriangles()}
             <div
-              className={`relative border-[4px] border-[var(--card-accent-color)] p-5 text-center ${varBorderRadius}`}
+              className={`items-center overflow-clip border-[4px] border-[var(--card-accent-color)] p-10 text-center ${varBorderRadius}`}
             >
-              <p className="text-center text-[1rem] font-bold uppercase">11:00 PM</p>
+              <p className="text-center text-[1.3rem] font-bold uppercase">2:00 PM</p>
               <h1 className="text-center text-[2rem] font-bold uppercase leading-[1.25] lg:text-[3rem]">
-                Closing Remarks
+                Sprint Ends
               </h1>
-              <p>
-                Designers will begin with registration and sign-in, an introduction to the event,
-                and energizing lighting talks.
-              </p>
+              <p>Teams will present their designs to our panel of judges.</p>
+            </div>
+          </div>
+          {/* Item 4 */}
+          <div className={`relative mb-[5%] w-full bg-[var(--card-primary-color)]`}>
+            {renderAccentTriangles()}
+            <div
+              className={`items-center overflow-clip border-[4px] border-[var(--card-accent-color)] p-10 text-center ${varBorderRadius}`}
+            >
+              <p className="text-center text-[1.3rem] font-bold uppercase">3:00 PM</p>
+              <h1 className="text-center text-[2rem] font-bold uppercase leading-[1.25] lg:text-[3rem]">
+                Finalist Revealed
+              </h1>
+              <p>Finalist presentations will begin.</p>
+            </div>
+          </div>
+          {/* Item 5 */}
+          <div className={`relative mb-[5%] w-full bg-[var(--card-primary-color)]`}>
+            {renderAccentTriangles()}
+            <div
+              className={`items-center overflow-clip border-[4px] border-[var(--card-accent-color)] p-10 text-center ${varBorderRadius}`}
+            >
+              <p className="text-center text-[1.3rem] font-bold uppercase">3:30 PM</p>
+              <h1 className="text-center text-[2rem] font-bold uppercase leading-[1.25] lg:text-[3rem]">
+                Closing Ceremony
+              </h1>
+              <p>3 winners will be announced and prizes will be announced.</p>
             </div>
           </div>
         </div>
@@ -201,9 +192,9 @@ export default function AgendaPage({ ref, showExpanded, borderStyle }: AgendaPag
       <PageTitle title="Agenda" subtitle="What’s the timeline of our design sprint?" />
       <div className="flex h-full w-full flex-col items-center pt-5">
         {/* Days */}
-        <div className="h-[15%] w-full flex-row justify-center p-5 sm:max-w-[90%] md:max-w-[50%]">
+        <div className="h-[200px] w-full flex-row justify-center p-5 sm:max-w-[90%] md:max-w-[50%]">
           <button
-            className={`${activeDay === 1 ? 'relative bg-[var(--card-button-color)] duration-500 ease-in-out' : 'relative bg-transparent duration-500 ease-in-out'} h-full w-1/2 ${varBorderRadius}`}
+            className={`${activeDay === 1 ? 'relative bg-[var(--card-button-color)] duration-500 ease-in-out' : 'bg-transparent duration-500 ease-in-out'} h-[150px] w-1/2 ${varBorderRadius}`}
             onClick={() => setActiveDay(1)}
           >
             {renderPrimaryTriangles()}
@@ -211,7 +202,7 @@ export default function AgendaPage({ ref, showExpanded, borderStyle }: AgendaPag
             <p>7:00pm - 11:00pm</p>
           </button>
           <button
-            className={`${activeDay === 2 ? 'relative bg-[var(--card-button-color)] duration-500 ease-in-out' : 'bg-transparent duration-500 ease-in-out'} h-full w-1/2 ${varBorderRadius}`}
+            className={`${activeDay === 2 ? 'relative bg-[var(--card-button-color)] duration-500 ease-in-out' : 'bg-transparent duration-500 ease-in-out'} h-[150px] w-1/2 ${varBorderRadius}`}
             onClick={() => setActiveDay(2)}
           >
             {renderPrimaryTriangles()}
